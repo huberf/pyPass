58133edfb4acaded7d8b62108ef24209ef22a183
Noah Huber-Feely <nhuberfeely@gmail.com>
2016-09-26 08:59:53 -0500

    Add script to add new passwords, and remove 'EOF' line

---
+++ add.sh
@@ -0,0 +1,4 @@
+echo "" >> c0d3sPl41n.txt
+echo "T: $1" >> c0d3sPl41n.txt
+echo "E: $2" >> c0d3sPl41n.txt
+echo "P: $3" >> c0d3sPl41n.txt
--- decrypt.py
+++ decrypt.py
@@ -7,14 +7,7 @@ except:
 import getpass
 import sys
 
-firstText = getpass.getpass()
-secondText = getpass.getpass("Re-enter Password:")
-
-if firstText == secondText:
-  key = encrypter.padKey( firstText )
-else:
-  print "Inputs didn't match."
-  sys.exit()
+key = encrypter.padKey( getpass.getpass() )
 
 cipherFile = open("c0d3sC1ph3r.txt", 'r')
 cipherText = cipherFile.read()
